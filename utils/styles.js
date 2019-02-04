a2d098354d1d6fcb2073b77197bfca933b2f7d89
Stefan <Stefan.Maretic@shareit.rs>
2019-02-04 13:16:52 +0100

    Add style rules util file

---
+++ utils/styles.js
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    black: '#000',
+    blue: '#00448f',
+    white: '#fff',
+    borderBlue: '#36a1fb',
+    textColor: '#3a3a3a',
+    containerGray: '#f6f6f6',
+    formLabel: '#0255B6',
+    formInput: '#4a4a4a',
+  },
+  breakpoints: {
+    lg: '1023px',
+  },
+  fonts: {
+    raleway: `'Raleway', sans-serif`,
+    openSans: `'Open Sans', sans-serif`,
+    montserrat: `'Montserrat', sans-serif`,
+  },
+};
