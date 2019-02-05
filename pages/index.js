9be9bcba2a139925f17846dc047c093fd33c77de
Stefan <Stefan.Maretic@shareit.rs>
2019-02-05 15:51:49 +0100

    Update index page

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,18 @@
  * will import it everywhere because of eslint errors.
  */
 import React from 'react';
-import Header from '../components/Header/';
+import Hero from '../components/Hero';
+import Layout from '../components/Layout';
 
-const Index = () => <Header />;
+const Index = () => (
+  <Layout title="Share IT Web Development">
+    <Hero
+      imgSrc="/static/banner.jpg"
+      title="Enterprise<br/> Software<br/> Development"
+      textColor="light"
+      uppercase
+    />
+  </Layout>
+);
 
 export default Index;
