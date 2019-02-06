cab9b70684f47dad2d9bd3de89d98aace3e1d761
Sasa Gvozdenovic <sasagvozdenovic@hotmail.com>
2019-02-06 12:11:27 +0100

    Removing comments

--- components/Footer/FooterLine.js
+++ components/Footer/FooterLine.js
@@ -56,15 +56,3 @@ class FooterLine extends Component {
 }
 
 export default FooterLine;
-
-/*
-<a href="https://www.facebook.com/Share-IT-doo-1990128854600049/">
-<img src="./static/social-facebook.png" alt="Facebook" />
-</a>
-<a href="https://twitter.com/stevabebic/">
-<img src="./static/social-twitter.png" alt="Twitter" />
-</a>
-<a href="https://www.linkedin.com/company/shareit/">
-<img src="./static/social-linkedin.png" alt="LinkedIn" />
-</a>
- */
