1b5871bd1f3d7538c6795057d902350460e2eb28
Stefan <Stefan.Maretic@shareit.rs>
2019-02-07 14:43:23 +0100

    Add scrollToTop to Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
 import Header from './Header/';
+import ScrollToTop from '../components/ScrollToTop';
 
 const Layout = ({ children, title }) => (
   <>
     <Head>
       <title>{title}</title>
     </Head>
+    <ScrollToTop />
     <Header />
     {children}
     {/* FOOTER GOES HERE */}
